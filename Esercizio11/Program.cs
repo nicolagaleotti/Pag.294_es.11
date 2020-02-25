a2e73c455d2e4c55c4d69b853b687fc4ef0ca034
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-25 09:13:36 +0100

    Add project files.

---
+++ Esercizio11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Metodo;
+
+namespace Esercizio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            Console.WriteLine("Inserisci la lunghezza dell'array: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            int numero;
+            for (int i = 0; i < a.Length; i++)
+            {
+                numero = r.Next(1, 50);
+                a[i] = numero;
+            }
+            Console.WriteLine("Array randomico:");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i]+" ");
+            }
+            a = CountingSort.Sort(a);
+            Console.WriteLine("\nArray ordinato:");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i]+" ");
+            }
+            Console.WriteLine("\nLa complessità è n^2!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Metodo/CountingSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Metodo
+{
+    public class CountingSort
+    {
+        public static int[] Sort(int[] array)
+        {
+            int[] array2 = new int[array.Length];
+            int c = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int y = 0; y < array.Length; y++)
+                {
+                    if (array[i] > array[y])
+                    {
+                        c++;
+                    }
+                }
+                array2[c] = array[i];
+                c = 0;
+            }
+            for (int i = 0; i < array2.Length; i++)
+            {
+                if (array2[i] == 0)
+                {
+                    array2[i] = array2[i - 1];
+                }
+            }
+            return array2;
+        }
+    }
+}
